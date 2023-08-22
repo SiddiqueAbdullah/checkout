9b807bafd93f272cea7dc5624dc317b3e6e08259
Abdullah Siddique <abdullah.siddique115@gmail.com>
2023-08-22 19:06:40 +0100

    Added mock for ProfuctRepository

---
+++ Checkout/Checkout.Tests/Mocks/MockedProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Checkout.Models.Interfaces;
+using Checkout.Repositories.Interfaces;
+using Checkout.Models;
+
+namespace Checkout.Tests.Mocks
+{
+    public class MockedProductRepository : IProductRepository
+    {
+        public IEnumerable<IProduct> Products { get;  }
+
+        public MockedProductRepository()
+        {
+            Products = new List<IProduct>
+            {
+                new Product("A").AddPriceRule(1, 50).AddPriceRule(3, 130),
+                new Product("B").AddPriceRule(1, 30).AddPriceRule(2, 45),
+                new Product("C").AddPriceRule(1, 20),
+                new Product("D").AddPriceRule(1, 15)
+            };
+        }
+
+        public IProduct FindBySKU(string sKU)
+        {
+            return Products.First(p => p.SKU == sKU);
+        }
+    }
+}
