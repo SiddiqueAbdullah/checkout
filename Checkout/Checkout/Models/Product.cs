1644a4d03dbe2140196dd00ef672b0cd9d39bbd3
Abdullah Siddique <abdullah.siddique115@gmail.com>
2023-08-22 16:55:35 +0100

    Added Database class and models

---
+++ Checkout/Checkout/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Checkout.Models
+{
+    public class Product
+    {
+        public string SKU { get; set; }
+        public int UnitPrice { get; set; }
+
+        public Product(string sKU, int unitPrice)
+        {
+            SKU = sKU;
+            UnitPrice = unitPrice;
+        }
+    }
+}
---
+++ Checkout/Checkout/Models/SpecialPrice.cs
@@ -0,0 +1,14 @@
+﻿namespace Checkout.Models
+{
+    public class SpecialPrice
+    {
+        public int RequiredQuantity { get; set; }
+        public int Price { get; set; }
+
+        public SpecialPrice(int requiredQuantity, int price)
+        {
+            RequiredQuantity = requiredQuantity;
+            Price = price;
+        }
+    }
+}
