b882c4e3f25399e92ca32acdbb452b475bfec94b
Abdullah Siddique <abdullah.siddique115@gmail.com>
2023-08-22 19:06:13 +0100

    Added PriceRule, and Product models

--- Checkout/Checkout/Models/PriceRule.cs
+++ Checkout/Checkout/Models/PriceRule.cs
@@ -1,14 +1,14 @@
-﻿namespace Checkout.Models
+﻿using Checkout.Models.Interfaces;
+
+namespace Checkout.Models
 {
-    public class SpecialPrice
+    internal class PriceRule : IPriceRule
     {
-        public string Item { get; set; }
         public int RequiredQuantity { get; set; }
         public int Price { get; set; }
 
-        public SpecialPrice(string item, int requiredQuantity, int price)
+        public PriceRule(int requiredQuantity, int price)
         {
-            Item = item;
             RequiredQuantity = requiredQuantity;
             Price = price;
         }
--- Checkout/Checkout/Models/Product.cs
+++ Checkout/Checkout/Models/Product.cs
@@ -1,14 +1,38 @@
-﻿namespace Checkout.Models
+﻿using Checkout.Models.Interfaces;
+
+namespace Checkout.Models
 {
-    public class Product
+    public class Product : IProduct
     {
         public string SKU { get; set; }
-        public int UnitPrice { get; set; }
+        public IEnumerable<IPriceRule> PriceRules { get; set; }
 
-        public Product(string sKU, int unitPrice)
+        public Product(string sKU)
         {
             SKU = sKU;
-            UnitPrice = unitPrice;
+            PriceRules = new List<PriceRule>();
+        }
+
+        public int GetPriceByQuantity(int quantity)
+        {
+            var unitPrice = PriceRules.First(pr => pr.RequiredQuantity == 1).Price;
+            var qualifiedPriceRule = PriceRules.FirstOrDefault(pr => pr.RequiredQuantity > 1 && pr.RequiredQuantity < quantity);
+
+            if(qualifiedPriceRule == null)
+            {
+                return unitPrice * quantity;
+            }
+
+            return (quantity / qualifiedPriceRule.RequiredQuantity) * qualifiedPriceRule.Price + (quantity % qualifiedPriceRule.RequiredQuantity) * unitPrice;
+        }
+
+        public IProduct AddPriceRule(int requiredQuantity, int price)
+        {
+            ((List<PriceRule>)PriceRules).Add(new PriceRule(requiredQuantity, price));
+
+            return this;
         }
     }
+
+    
 }
